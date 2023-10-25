0c1c9913cc2948c4790455c614f9d6169a1a579d
aaronkc117@gmail.com <kcarjun@kcs-MacBook-Air.local>
2023-10-25 14:24:20 +0545

    Latest change 7

---
+++ scripts/12-todo-list.js
@@ -0,0 +1,53 @@
+
+let todolist = JSON.parse(localStorage.getItem('todoList')) || [];
+
+
+renderTodoList();
+
+
+
+function renderTodoList(){
+    let todoListHTML = '';
+    todolist.forEach(function(todoObject,index){
+      //const name = todoObject.name;
+      ///const dueDate = todoObject.dueDate;
+      const { name, dueDate} = todoObject;
+      const html = `
+      <div>${name}</div>
+      <div>${dueDate}</div>
+      <button onclick="
+        todolist.splice(${index}, 1)
+        renderTodoList();
+      " class="delete-button">Delete</button>  
+      `
+      todoListHTML += html;
+    })
+document.querySelector('.js-todo-list')
+  .innerHTML = todoListHTML;
+
+  localStorage.setItem('todoList',JSON.stringify(todolist));
+
+
+ 
+}
+
+
+function addTodo(){
+  const inputElement = document.querySelector('.js-name-input');
+  const name = inputElement.value;
+
+  const dueDateElement = document.querySelector('.js-date-input');
+  const dueDate = dueDateElement.value;
+
+  todolist.push({
+    //name: name,
+    //dueDate: dueDate,
+    name ,
+    dueDate});
+
+  inputElement.value = '';
+
+  renderTodoList();
+}
+
+
